6219e765e152679cac5ebc1d2e5b37455e1172a7
Graham Thomas <thom1229@msu.edu>
2018-08-08 08:31:24 -0700

    Copies server folder from angular website to react website (as it functions the same)

---
+++ server/models/booking.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+  startAt: { type: Date, required: "Starting Date is Required"},
+  endAt: {type: Date, required: "Ending Date is Required"},
+  totalPrice: Number,
+  days: Number,
+  guests: Number,
+  createdAt: {type: Date, default: Date.now},
+  user: {type: Schema.Types.ObjectId, ref: "User"},
+  rental: {type: Schema.Types.ObjectId, ref: "Rental"}
+});
+
+
+module.exports = mongoose.model("Booking", bookingSchema);
